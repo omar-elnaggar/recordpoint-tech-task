bd76170edf57f60c856700d9a3a1b4c7b5074137
omar.elnaggar <elnaggaros@gmail.com>
2022-02-18 01:00:04 -0800

    added model and pgsql info

--- name-wall/app.py
+++ name-wall/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from models import db, Name
 
 app = Flask(__name__)
 app.secret_key = "something_secret"
+app.config['SQLALCHEMY_DATABASE_URI'] =  'postgresql://pguser:pgpass@postgres/names'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        name = request.form['nm']
-        flash(f'Thank you for signing your name {name}!', 'success')
+        new_name = Name(name=request.form['nm'])
+        db.session.add(new_name)
+        db.session.commit()
+        flash(f'Thank you for signing your name {new_name.name}!', 'success')
         return render_template('index.html')
     else:
         return render_template('index.html')
+
+    
+def get_names():
+    return db.session.query.all()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ name-wall/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+ 
+db = SQLAlchemy()
+
+class Name(db.Model):
+    __tablename__ = 'names'
+
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'{self.name}'
