28dc81ba51e706a98bfb8e001641882c35587e26
omar.elnaggar <elnaggaros@gmail.com>
2022-02-18 00:24:51 -0800

    added form and flash messages with post method

--- name-wall/app.py
+++ name-wall/app.py
@@ -1,10 +1,17 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "something_secret"
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        name = request.form['nm']
+        flash(f'Thank you for signing your name {name}!', 'success')
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
